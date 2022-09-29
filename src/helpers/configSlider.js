120c8aa8382e51b52648c75b9adccfdff14261be
b0pez <blopez4434@gmail.com>
2022-09-29 11:12:42 -0300

    configurando estilos

--- src/helpers/configSlider.js
+++ src/helpers/configSlider.js
@@ -6,4 +6,29 @@ export const settings = {
     autoplay: true,
     autoplaySpeed: 2000,
     pauseOnHover: true,
+    initialSlide: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 };
